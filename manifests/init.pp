42fccf063db925055cb21bcee1f0a414c1ff3912
Alexander Paetz <alexander.paetz@noris.de>
2018-03-22 16:49:19 +0100

    main class does not need paramaters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,8 @@
 # pnp4nagios class
 
-class pnp4nagios(
-  
-){
+class pnp4nagios {
 
   class { 'pnp4nagios::package': } ->
   class { 'pnp4nagios::config': } ->
   class { 'pnp4nagios::service': }
-
 }
