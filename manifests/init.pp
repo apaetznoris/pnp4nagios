92485d85b0bb7e65651b891a0bb0ea9183ec3a30
Alexander Paetz <alexander.paetz@noris.de>
2018-03-23 11:51:48 +0100

    use config class parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,12 @@
 # pnp4nagios class
 
-class pnp4nagios {
+class pnp4nagios(
+  $pnp_config = {}
+) {
 
   class { 'pnp4nagios::package': } ->
-  class { 'pnp4nagios::config': } ->
+  class { 'pnp4nagios::config': 
+    config => $pnp_config
+  } ->
   class { 'pnp4nagios::service': }
 }
